2c76000d5d9a73ded54887993344f6f04e12051e
Gernot Roetzer <gernot.roetzer@gmail.com>
2021-03-05 00:01:22 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tf2_fm_zoo",
+    version="0.0.1",
+    url="https://github.com/RoetGer/tf2-fm-zoo",
+    packages=find_packages(include=["fm_zoo"])
+)
