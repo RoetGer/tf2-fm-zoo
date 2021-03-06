fdf4c8015ea9739f4947e34bc960db50f022db98
Gernot Roetzer <gernot.roetzer@gmail.com>
2021-03-06 23:39:19 +0100

    Fix import problem.

--- fm_zoo/__init__.py
+++ fm_zoo/__init__.py
@@ -1,12 +1,18 @@
 import glob
-import os as osp
+import importlib
+import os.path as osp
 
 
-# Import all the modules in the directory
+## Import all the modules in the directory
+
 glob_str = osp.join(osp.dirname(__file__), "*.py")
 modules = glob.glob(glob_str)
 
-__all__ = [ 
+# Clean up module names
+modules = [ 
     osp.basename(f).replace(".py", "") 
         for f in modules if osp.isfile(f) and not f.endswith("__init__.py")
 ]
+
+for module_name in modules:
+    importlib.import_module(f"fm_zoo.{module_name}")
