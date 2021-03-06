51368c1bb90fd8400c63bc7d7b3be98105ef09d4
Gernot Roetzer <gernot.roetzer@gmail.com>
2021-03-06 23:08:09 +0100

    Alias os.path with osp in __init__.py

--- fm_zoo/__init__.py
+++ fm_zoo/__init__.py
@@ -1,12 +1,12 @@
 import glob
-import os
+import os as osp
 
 
 # Import all the modules in the directory
-glob_str = os.path.join(dirname(__file__), "*.py")
+glob_str = osp.join(osp.dirname(__file__), "*.py")
 modules = glob.glob(glob_str)
 
 __all__ = [ 
-    os.path.basename(f).replace(".py", "") 
-        for f in modules if os.path.isfile(f) and not f.endswith("__init__.py")
+    osp.basename(f).replace(".py", "") 
+        for f in modules if osp.isfile(f) and not f.endswith("__init__.py")
 ]
