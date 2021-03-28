bab4a3e145d617ba06096bbecd3b182e6778f328
Gernot Roetzer <gernot.roetzer@gmail.com>
2021-03-28 23:58:07 +0200

    Allow to set a regularizing prior distribution for the weights used in the embeddings of the basic FactorizationMachine.

--- fm_zoo/fm.py
+++ fm_zoo/fm.py
@@ -11,13 +11,19 @@ class FactorizationMachine(tf.keras.Model):
 
     Degree d here is fixed at 2, so that we can simplify the calculation. Only hyperparam is factor_dim
 
+    The paramer ´prior´ allows to set a prior distribution for the trained weights of the embeddings,
+    this permits regularization of the weights.
+
     The input is discretized/Integer Encoded instead of real valued.
     See common.EmbedFeatures for detail on input configs.
    """
-    def __init__(self, feature_cards, factor_dim, name='factorization_machine'):
+    def __init__(self, feature_cards, factor_dim, prior=None, name='factorization_machine'):
         super(FactorizationMachine, self).__init__(name=name)
         self.embedding = EmbedFeatures(feature_cards, factor_dim, name=name + '/feature_embedding')
         self.linear = LinearModel(feature_cards, name=name + '/linear_model')
+        
+        if prior:
+            self.prior = prior
 
     def call(self, x, training=False):
         linear_out = self.linear(x)
@@ -25,4 +31,17 @@ class FactorizationMachine(tf.keras.Model):
         sum_of_squares = tf.reduce_sum(tf.pow(factors, 2), 1)
         square_of_sums = tf.pow(tf.reduce_sum(factors, 1), 2)
         interaction_out = 0.5 * tf.reduce_sum(square_of_sums - sum_of_squares, 1, keepdims=True)
+        
+        if self.prior:
+            neg_prior_linear = - tf.reduce_sum(
+                self.prior.log_prob(
+                    self.linear.linear.embeddings)
+            )
+            neg_prior_embedd = - tf.reduce_sum(
+                self.prior.log_prob(
+                    self.embedding.embedding.embeddings)
+            )
+
+            self.add_loss(neg_prior_linear + neg_prior_embedd)
+        
         return linear_out + interaction_out
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(osp.join(rep_folder, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="tf2_fm_zoo",
-    version="0.1.2",
+    version="0.2.0",
     url="https://github.com/RoetGer/tf2-fm-zoo",
     packages=find_packages(include=["fm_zoo"]),
     long_description=long_description, 
